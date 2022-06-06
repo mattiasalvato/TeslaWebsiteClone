233a3021417c2656d871613e350707f08d3be15a
Mattia Salvato <mattiasalvatosg@gmail.com>
2022-06-06 14:40:48 +0200

    linkMenu

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,8 +1,8 @@
 <template>
 
-  <header class="fixed w-[100%]">
-    <a href="">
-      <img src="../assets/images/tesla_logo.svg" alt=""  class="absolute w-[10rem] p-6 
+  <header class="fixed w-[100%]"  id="index">
+    <a href="index">
+      <img  src="../assets/images/tesla_logo.svg" alt=""  class="absolute w-[10rem] p-6 
                                                                 lg:w-[10rem] lg:ml-4 ">
     </a>
 
@@ -38,12 +38,12 @@
           <svg class="absolute w-3 mt-10 right-10 opacity-75" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M376.6 427.5c11.31 13.58 9.484 33.75-4.094 45.06c-5.984 4.984-13.25 7.422-20.47 7.422c-9.172 0-18.27-3.922-24.59-11.52L192 305.1l-135.4 162.5c-6.328 7.594-15.42 11.52-24.59 11.52c-7.219 0-14.48-2.438-20.47-7.422c-13.58-11.31-15.41-31.48-4.094-45.06l142.9-171.5L7.422 84.5C-3.891 70.92-2.063 50.75 11.52 39.44c13.56-11.34 33.73-9.516 45.06 4.094L192 206l135.4-162.5c11.3-13.58 31.48-15.42 45.06-4.094c13.58 11.31 15.41 31.48 4.094 45.06l-142.9 171.5L376.6 427.5z"/></svg>
       </div>
       <ul class="grid gap-y-2 mt-[90px] mr-[80px] ml-10 font-semibold text-gray-600 text-[15px] mb-80 capitalize">
-        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="">Model 3</a></li>
-        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="">Model S</a></li>
-        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="">Model X</a></li>
-        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="">Model Y</a></li>
-        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="">Solar Roof</a></li>
-        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="">Solar Panels</a></li>
+        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="#Model3">Model 3</a></li>
+        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="#ModelS">Model S</a></li>
+        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="#ModelX">Model X</a></li>
+        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="#ModelY">Model Y</a></li>
+        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="#SolarRoof">Solar Roof</a></li>
+        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="#SolarPanel">Solar Panels</a></li>
         <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="">Existing Inventory</a></li>
         <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="">Used Inventory</a></li>
         <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="">Trade-In</a></li>
@@ -71,7 +71,7 @@
   
     <div id="section">
       <div id="content" class="text-center pt-[100px]">
-        <h1 class="text-4xl font-semibold md:text-5xl">Model 3</h1>
+        <h1 id="Model3" class="text-4xl font-semibold md:text-5xl">Model 3</h1>
         <h2 class="mt-[5px] capitalize">Order Online for <a href="" class="underline">Touchless Delivery</a></h2>
 
           <div class="mt-[300px] w-[100%] flex flex-wrap justify-center sm:grid grid-cols-2 md:grid grid-cols-2 md:mt-[400px]">
@@ -88,7 +88,7 @@
 
     <div id="section">
       <div id="content" class="text-center pt-[100px]">
-        <h1 class="text-4xl font-semibold md:text-5xl">Model 3</h1>
+        <h1 id="ModelY" class="text-4xl font-semibold md:text-5xl">Model Y</h1>
         <h2 class="mt-[5px] capitalize">Order Online for <a href="" class="underline">Touchless Delivery</a></h2>
 
           <div class="mt-[300px] w-[100%] flex flex-wrap justify-center md:grid grid-cols-2 md:mt-[400px]">
@@ -106,7 +106,7 @@
  
     <div id="section"> 
       <div id="content" class="text-center pt-[100px]">
-        <h1 class="text-4xl font-semibold md:text-5xl">Model S</h1>
+        <h1 id="ModelS" class="text-4xl font-semibold md:text-5xl">Model S</h1>
         <h2 class="mt-[5px] capitalize">Order Online for <a href="" class="underline">Touchless Delivery</a></h2>
 
         <div class="mt-[300px] w-[100%] flex flex-wrap justify-center md:grid grid-cols-2 md:mt-[400px]">
@@ -121,7 +121,7 @@
 
     <div id="section">
       <div id="content"  class="text-center pt-[100px]">
-        <h1 class="text-4xl font-semibold">Model X</h1>
+        <h1 id="ModelX" class="text-4xl font-semibold">Model X</h1>
         <h2 class="mt-[5px] capitalize">Order Online for <a href="" class="underline">Touchless Delivery</a></h2>
 
         <div class="mt-[300px] w-[100%] flex flex-wrap justify-center md:grid grid-cols-2 md:mt-[400px]">
@@ -137,7 +137,7 @@
 
     <div id="section">
       <div id="content" class="text-center pt-[100px]">
-        <h1 class="text-4xl font-semibold">Solar Panel</h1>
+        <h1 id="SolarPanel" class="text-4xl font-semibold">Solar Panel</h1>
         <h2 class="mt-[5px] capitalize">Lowest Cost Solar Panels in America</h2>
 
         <div class="mt-[300px] w-[100%] flex flex-wrap justify-center md:grid grid-cols-2 md:mt-[400px]">
@@ -152,7 +152,7 @@
 
     <div id="section">
       <div id="content" class="text-center pt-[100px]">
-        <h1 class="text-4xl font-semibold">Solar Roof</h1>
+        <h1 id="SolarRoof" class="text-4xl font-semibold">Solar Roof</h1>
         <h2 class="mt-[5px] capitalize">Produce Clean Energy From Your Roof</h2>
 
         <div class="mt-[300px] w-[100%] flex flex-wrap justify-center md:grid grid-cols-2 md:mt-[400px]">
@@ -166,7 +166,7 @@
 
     <div id="section">
       <div id="content"  class="text-center pt-[100px]">
-        <h1 class="text-4xl font-semibold">Accessories</h1>
+        <h1 id="Accessories" class="text-4xl font-semibold">Accessories</h1>
 
         <div class="mt-[375px] w-[100%] text-center flex flex-wrap justify-center md:grid grid-cols-1 md:mt-[420px]">
             <a href="" class="basis-[80%] bg-zinc-900 text-white  font-semibold text-sm m-2 p-[10px] rounded-full
@@ -199,6 +199,7 @@ export default {
     displayMenu(){
 
       this.menuActive = !this.menuActive
+      
     
     }
   }
