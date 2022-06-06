320348b88aacf96a714ed685dc8684ed9756db8c
Mattia Salvato <mattiasalvatosg@gmail.com>
2022-06-06 14:49:26 +0200

    menu links

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -38,30 +38,30 @@
           <svg class="absolute w-3 mt-10 right-10 opacity-75" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M376.6 427.5c11.31 13.58 9.484 33.75-4.094 45.06c-5.984 4.984-13.25 7.422-20.47 7.422c-9.172 0-18.27-3.922-24.59-11.52L192 305.1l-135.4 162.5c-6.328 7.594-15.42 11.52-24.59 11.52c-7.219 0-14.48-2.438-20.47-7.422c-13.58-11.31-15.41-31.48-4.094-45.06l142.9-171.5L7.422 84.5C-3.891 70.92-2.063 50.75 11.52 39.44c13.56-11.34 33.73-9.516 45.06 4.094L192 206l135.4-162.5c11.3-13.58 31.48-15.42 45.06-4.094c13.58 11.31 15.41 31.48 4.094 45.06l-142.9 171.5L376.6 427.5z"/></svg>
       </div>
       <ul class="grid gap-y-2 mt-[90px] mr-[80px] ml-10 font-semibold text-gray-600 text-[15px] mb-80 capitalize">
-        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="#Model3">Model 3</a></li>
-        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="#ModelS">Model S</a></li>
-        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="#ModelX">Model X</a></li>
-        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="#ModelY">Model Y</a></li>
-        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="#SolarRoof">Solar Roof</a></li>
-        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="#SolarPanel">Solar Panels</a></li>
-        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="">Existing Inventory</a></li>
-        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="">Used Inventory</a></li>
-        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="">Trade-In</a></li>
-        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="">Test Drive</a></li>
-        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="">Insurance</a></li>
-        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="">Cybertruck</a></li>
-        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="">Roadster</a></li>
-        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="">Semi</a></li>
-        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="">Chargin</a></li>
-        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="">Powerwall</a></li>
-        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="">Commercial Energy</a></li>
-        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="">Utilities</a></li>
-        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="">Find Us</a></li>
-        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="">Support</a></li>
-        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="">Investor Relations</a></li>
-        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="">Shop</a></li>
-        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="">Account</a></li>
-        <li class="hover:bg-gray-100 p-[7px] rounded-2xl"><a href="">More</a></li>
+        <li class="hover:bg-gray-100 p-[7px] rounded-2xl">Model 3</li>
+        <li class="hover:bg-gray-100 p-[7px] rounded-2xl">Model S</li>
+        <li class="hover:bg-gray-100 p-[7px] rounded-2xl">Model X</li>
+        <li class="hover:bg-gray-100 p-[7px] rounded-2xl">Model Y</li>
+        <li class="hover:bg-gray-100 p-[7px] rounded-2xl">Solar Roof</li>
+        <li class="hover:bg-gray-100 p-[7px] rounded-2xl">Solar Panels</li>
+        <li class="hover:bg-gray-100 p-[7px] rounded-2xl">Existing Inventory</li>
+        <li class="hover:bg-gray-100 p-[7px] rounded-2xl">Used Inventory</li>
+        <li class="hover:bg-gray-100 p-[7px] rounded-2xl">Trade-In</li>
+        <li class="hover:bg-gray-100 p-[7px] rounded-2xl">Test Drive</li>
+        <li class="hover:bg-gray-100 p-[7px] rounded-2xl">Insurance</li>
+        <li class="hover:bg-gray-100 p-[7px] rounded-2xl">Cybertruck</li>
+        <li class="hover:bg-gray-100 p-[7px] rounded-2xl">Roadster</li>
+        <li class="hover:bg-gray-100 p-[7px] rounded-2xl">Semi</li>
+        <li class="hover:bg-gray-100 p-[7px] rounded-2xl">Chargin</li>
+        <li class="hover:bg-gray-100 p-[7px] rounded-2xl">Powerwall</li>
+        <li class="hover:bg-gray-100 p-[7px] rounded-2xl">Commercial Energy</li>
+        <li class="hover:bg-gray-100 p-[7px] rounded-2xl">Utilities</li>
+        <li class="hover:bg-gray-100 p-[7px] rounded-2xl">Find Us</li>
+        <li class="hover:bg-gray-100 p-[7px] rounded-2xl">Support</li>
+        <li class="hover:bg-gray-100 p-[7px] rounded-2xl">Investor Relations</li>
+        <li class="hover:bg-gray-100 p-[7px] rounded-2xl">Shop</li>
+        <li class="hover:bg-gray-100 p-[7px] rounded-2xl">Account</li>
+        <li class="hover:bg-gray-100 p-[7px] rounded-2xl">More</li>
       </ul>
     </div>
 
